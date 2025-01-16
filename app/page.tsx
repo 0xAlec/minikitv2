d563ec043227888b91f73fa2b063752cb0b67ca9
Alec Chen <fkalec@gmail.com>
2025-01-16 15:06:16 -0500

    login

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,14 @@
 import { WalletIsland } from '@coinbase/onchainkit/wallet';
 import { TransactionDefault } from '@coinbase/onchainkit/transaction';
 import { useEffect, useState } from 'react';
+import { useLoginToFrame } from "@privy-io/react-auth/farcaster";
 import sdk from '@farcaster/frame-sdk';
+import { usePrivy } from '@privy-io/react-auth';
+import { Loader } from './components/Loader';
 
 export default function App() {
+  const { ready, authenticated } = usePrivy();
+  const { initLoginToFrame, loginToFrame } = useLoginToFrame();
   const [isSDKLoaded, setIsSDKLoaded] = useState(false);
 
   useEffect(() => {
@@ -19,11 +24,29 @@ export default function App() {
   }, [isSDKLoaded]);
 
   useEffect(() => {
+    if (ready && !authenticated) {
+      const login = async () => {
+        const { nonce } = await initLoginToFrame();
+        const result = await sdk.actions.signIn({ nonce: nonce });
+        await loginToFrame({
+          message: result.message,
+          signature: result.signature,
+        });
+      };
+      login();
+    }
+  }, [ready, authenticated, initLoginToFrame, loginToFrame]);
+
+  useEffect(() => {
     import('eruda').then((eruda) => {
       eruda.default.init();
       window.console = eruda.default.get('console');
     });
   }, []);
+
+  if (!ready || !isSDKLoaded || !authenticated) {
+    return <Loader />;
+  }
 
   return (
     <div className="flex flex-col min-h-screen font-sans dark:bg-background dark:text-white bg-white text-black">
