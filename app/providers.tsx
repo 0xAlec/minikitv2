b8259bac7dd39540dc115dd59a3f4d98e3c5316c
Alec Chen <fkalec@gmail.com>
2025-01-16 17:51:43 -0500

    minikitprovider

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,55 +1,25 @@
 'use client';
 
-import { OnchainKitProvider } from '@coinbase/onchainkit';
 import type { ReactNode } from 'react';
-import {createConfig} from '@privy-io/wagmi';
-import {WagmiProvider} from '@privy-io/wagmi';
 import {baseSepolia} from 'viem/chains';
-import {http} from 'wagmi';
-import { PrivyProvider } from '@privy-io/react-auth';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import { MiniKitProvider } from '@/lib/config/MiniKitProvider';
 
-const config = createConfig({
-  chains: [baseSepolia],
-  transports: {
-    [baseSepolia.id]: http(),
-  },
-});
 
 export function Providers(props: { children: ReactNode }) {
-  const queryClient = new QueryClient();
 
   return (
-    <PrivyProvider
-    appId="cm4rmv3a804pq14ccraqzl5nz"
-    config={{
-      // Customize Privy's appearance in your app
-      appearance: {
-        theme: 'light',
-        accentColor: '#676FFF',
+    <MiniKitProvider
+      appId={'cm4rmv3a804pq14ccraqzl5nz'}
+      config={{
+        apiKey: 'ozpCtG8CfD3TIod_1Va7UBsUm5Rn1-XS',
+        name: 'Mini App Template',
         logo: 'https://onchainkit.xyz/favicon/48x48.png?v4-19-24',
-      },
-      loginMethods: ['telegram'],
-      embeddedWallets: {
-        createOnLogin: 'users-without-wallets',
-      },
-    }}
+        mode: 'auto',
+      }}
+      chain={baseSepolia}
     >
-      <QueryClientProvider client={queryClient}>
-      <WagmiProvider config={config}>
-        <OnchainKitProvider
-          apiKey={'ozpCtG8CfD3TIod_1Va7UBsUm5Rn1-XS'}
-          chain={baseSepolia}
-            config={{ appearance: { 
-              mode: 'auto',
-          }
-        }}
-        >
-          {props.children}
-        </OnchainKitProvider>
-        </WagmiProvider>
-      </QueryClientProvider>
-    </PrivyProvider>
+      {props.children}
+    </MiniKitProvider>
   );
 }
 
---
+++ lib/config/MiniKitProvider.tsx
@@ -0,0 +1,69 @@
+import { OnchainKitProvider } from "@coinbase/onchainkit"
+import { PrivyProvider } from "@privy-io/react-auth"
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactNode } from "react"
+import { createConfig, http, WagmiProvider } from "wagmi"
+import { baseSepolia, Chain } from "wagmi/chains"
+
+export type MiniKitProviderConfig = {
+    apiKey?: string,
+    name?: string,
+    logo?: string,
+    mode?: 'auto' | 'light' | 'dark',
+    theme?: string,
+}
+
+export function MiniKitProvider({
+    appId,
+    config,
+    chain = baseSepolia,
+    children,
+}: {
+    appId: string,
+    config?: MiniKitProviderConfig,
+    chain?: Chain,
+    children: ReactNode,
+}) {
+    const wagmiConfig = createConfig({
+        chains: [chain],
+        transports: {
+          [chain.id]: http(),
+        },
+      });      
+    const queryClient = new QueryClient();
+
+    return (
+        <PrivyProvider
+        appId={appId}
+        config={{
+          appearance: {
+            theme: 'light',
+            accentColor: '#676FFF',
+            logo: 'https://onchainkit.xyz/favicon/48x48.png?v4-19-24',
+          },
+          loginMethods: ['telegram'],
+          embeddedWallets: {
+            createOnLogin: 'users-without-wallets',
+          },
+        }}
+        >
+          <QueryClientProvider client={queryClient}>
+          <WagmiProvider config={wagmiConfig}>
+            <OnchainKitProvider
+              apiKey={config?.apiKey}
+              chain={chain}
+                config={{ appearance: { 
+                  name: config?.name,
+                  logo: config?.logo,
+                  mode: config?.mode,
+                  theme: config?.theme,
+              }
+            }}
+            >
+              {children}
+            </OnchainKitProvider>
+            </WagmiProvider>
+          </QueryClientProvider>
+        </PrivyProvider>    
+    )
+}
