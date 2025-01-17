566b19c6270dd40298d91acb943324bf870e4de5
Alec Chen <fkalec@gmail.com>
2025-01-16 19:07:34 -0500

    add chain

--- app/providers.tsx
+++ app/providers.tsx
@@ -12,11 +12,11 @@ export function Providers(props: { children: ReactNode }) {
       appId={'cm4rmv3a804pq14ccraqzl5nz'}
       config={{
         apiKey: 'ozpCtG8CfD3TIod_1Va7UBsUm5Rn1-XS',
+        chain: baseSepolia,
         name: 'Mini App Template',
         logo: 'https://onchainkit.xyz/favicon/48x48.png?v4-19-24',
         mode: 'auto',
       }}
-      chain={baseSepolia}
     >
       {props.children}
     </MiniKitProvider>
